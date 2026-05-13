e389a43e5d3b2520b3743fec6f66793abc45e793
Luam A. <luaminalem@gmail.com>
2026-05-13 23:14:56 +0300

    Add css loaders to webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -22,8 +22,14 @@ module.exports = {
                       options: {
                           presets: ['@babel/preset-react']
                       }
-                  }
+                  },
+                 {    
+                     test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                 }
+                  
               ]
+              
           }
       ]
   }
