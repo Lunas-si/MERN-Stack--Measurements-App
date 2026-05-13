191f85fd97776da2be7c7c4c422cdc12e8c169e9
Luam A. <luaminalem@gmail.com>
2026-05-14 00:11:07 +0300

    webpack HTMLPlugin added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -7,6 +8,11 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
